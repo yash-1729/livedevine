518d797f578a1e44f7e879588c144478d92880d0
yash-1729 <63740814+yash-1729@users.noreply.github.com>
2022-11-17 17:31:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,165 @@
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false,
+        },
+        1000: {
+          items: 3,
+          nav: true,
+        },
+      },
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+
+const slides = document.querySelectorAll(".slides");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const dots = document.querySelectorAll(".dot");
+
+let index = 0;
+
+// Adding opacity to first dot on first time
+
+dots[0].style.opacity = "1";
+
+// positioning the slides
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+// move slide function
+
+const moveSlide = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${index * 100}%)`;
+  });
+};
+
+// remove dots opacity 1 from all dots
+
+const removeDotsOpacity = () => {
+  dots.forEach((dot) => {
+    dot.style.opacity = ".2";
+  });
+};
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", (e) => {
+    index = i;
+    removeDotsOpacity();
+    e.target.style.opacity = "1";
+    moveSlide();
+  });
+});
+
+// show the previous slide
+
+// prevBtn.addEventListener("click", () => {
+//   if (index === 0) return index;
+//   index--;
+//   removeDotsOpacity();
+//   dots[index].style.opacity = "1";
+//   moveSlide();
+// });
+
+// show the next slide
+
+// nextBtn.addEventListener("click", () => {
+//   if (index === slides.length - 1) return index;
+//   index++;
+//   removeDotsOpacity();
+//   dots[index].style.opacity = "1";
+//   moveSlide();
+// });
+
+// auto play slide
+//
+
+const autoPlaySlide = () => {
+  removeDotsOpacity();
+  if (index === slides.length - 1) index =-1;
+  index++;
+  dots[index].style.opacity = "1";
+  moveSlide();
+};
+
+window.onload = () => {
+  setInterval(autoPlaySlide, 1000);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// navbar section 
+
+let isMenuOpen = false;
+const toggleMenu = () => {
+  if (isMenuOpen) {
+    // close it
+    document.getElementById("menu").classList.remove("translate-y-0");
+    document.getElementById("menu").classList.add("-translate-y-full");
+  } else {
+    document.getElementById("menu").classList.remove("-translate-y-full");
+    document.getElementById("menu").classList.add("translate-y-0");
+  }
+  isMenuOpen = !isMenuOpen;
+};
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xs:'300px',
+      sm:' 480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px',
+
+
+    },
+
+    fontfamily: {
+
+      RedHat:['Red Hat Display'],
+
+    }, 
+    extend: {
+        colors: {
+          'lightblack': '#171925',
+          'new': '#171925',
+          'lightblack': '#171925',
+          'lightblack': '#171925',
+          'lightblack': '#1B1D2A',
+        
+
+        }
+
+
+    },
+  },
+  plugins: [],
+}
